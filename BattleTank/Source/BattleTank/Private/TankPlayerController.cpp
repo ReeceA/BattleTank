7adbda939102f1809797e7e4fa4efd742de584fd
ReeceA <reeceatherton@yahoo.com>
2018-07-27 12:20:09 +0200

    BT08 - AI possesing Tank_BP

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetContolledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a Tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *(ControlledTank->GetName()))
+	}
+}
+
+ATank* ATankPlayerController::GetContolledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
